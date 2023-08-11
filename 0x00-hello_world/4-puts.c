819eaa4d6ba641a07884006884dad873132801da
Eucharia2023 <onuorahuk@gmail.com>
2023-08-10 18:23:18 -0700

    Adding string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,7 @@
-#include<stdio.h>
-/**
- * main - A program that prints a string
- *
- * return: 0 (success)
- * /
-int main(void
-{
-	puts("Programming is like building a multilingual puzzle"\n");
-	return(0);
+#include <stdio.h>
+
+int main(void) 
+{ 
+	puts("Programming is like building a multilingual puzzle\n"); 
+	return (0); 
 }
