d74375d0499d6183f88c6ec61983a2620176f476
Eucharia2023 <onuorahuk@gmail.com>
2023-08-13 14:12:30 -0700

    A program that prints in lower case using putchar function

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,14 +2,30 @@
 #include <time.h>
 #include <stdio.h> 
 
-/* betty style doc for function main goes there */
+/**
+ * main - Program that prints random number
+ *
+ * Return: 0 (sucess)
+*/
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-	/* your code goes there */
+	int lastDigit = n;
+	printf("Last digit of %d ", n);
+	if (lastDigit > 5)
+	{
+		printf("greater than 5\n", n);
+	}
+	else if (lastDigit == 0)
+	{
+		printf("%d and is 0\n", n);
+	}
+	else if (lastDigit < 6) && ! 0))
+	{
+		printf("%d and is less than 6 and not 0\n", n);
+	}
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print the alphabet in lower case
+ *
+ * return 0 (success)
+*/
+int main ()
+{
+	char ch;
+		for(ch = 'a'; ch <= 'z'; ch++)
+		{
+		putchar(ch);
+		putchar ('\n');
+		}
+	return 0;
+}	
