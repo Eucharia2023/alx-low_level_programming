9eb2f95a306ba2173db0002a89b3bd44171717ab
Eucharia2023 <onuorahuk@gmail.com>
2023-08-14 10:41:01 -0700

    Program that random numbers and giving info

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,6 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lastd = 0 % 10;
-	printf("Last digit of %d ", n);
 	if (lastd > 5)
 	{
 		printf("last digit of %d is %d and is greater than 5\n", n, lastd);
