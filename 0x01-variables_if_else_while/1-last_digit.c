01de7ae0e12de68c38bc9aefcc15e1499d55cd7d
Eucharia2023 <onuorahuk@gmail.com>
2023-08-14 10:33:58 -0700

    Program that random numbers and giving info

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,31 +1,32 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h> 
-
+#include <stdio.h>
 /**
- * main - Program that prints random number
+ * main - Program that prints random no and giving info abt it
  *
- * Return: 0 (sucess)
+ * Return: always (0)
+ *
 */
 int main(void)
 {
 	int n;
+	int lastd;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int lastDigit = n;
+	lastd = 0 % 10;
 	printf("Last digit of %d ", n);
-	if (lastDigit > 5)
+	if (lastd > 5)
 	{
-		printf("greater than 5\n", n);
+		printf("last digit of %d is %d and is greater than 5\n", n, lastd);
 	}
-	else if (lastDigit == 0)
+	else if (lastd == 0)
 	{
-		printf("%d and is 0\n", n);
+		printf("last digit of %d is %d and is 0\n", n, lastd);
 	}
-	else if (lastDigit < 6) && ! 0))
+	else
 	{
-		printf("%d and is less than 6 and not 0\n", n);
+		printf("last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
 	}
 	return (0);
 }
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,16 +6,11 @@
 */
 int main(void)
 {
-	/**
-	 * main - print the alphabet in lower case
-	 *
-	 * return 0 (success)
-	*/
 	char ch;
 		for (ch = 'a'; ch <= 'z'; ch++)
 		{
 		putchar(ch);
+		putchar("\n");
 		}
-	putchar("\n"); /*Print a newline after printing the alphabet*/
-	return (0); /*indicate sucess*/
+	return (0);
 }
