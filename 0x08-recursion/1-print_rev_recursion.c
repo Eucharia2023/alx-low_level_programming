9ce53f7db005e5906fb825df6da5f8e4268c2487
Eucharia2023 <onuorahuk@gmail.com>
2023-08-30 13:31:49 -0700

    Print a string backward

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - print a string followed by a new line
+ * @s: string
+ * Return: Always 0
+ *
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+	putchar(*s);
+	_print_rev_recursion(s + 1);
+}
